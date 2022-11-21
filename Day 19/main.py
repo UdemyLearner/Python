b3ef44e0de9ced6698ddc32e6294f05afd264fec
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-22 00:57:10 +0530

    modified:   Day 19/main.py

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,11 +1,35 @@
+import random
 import turtle as t
 
-tuktuk = t.Turtle()
+
 screen = t.Screen()
+screen.setup(width=500, height=400)
 
-def move_forward():
-    tuktuk.forward(10)
+user_bet = screen.textinput(title="make your bet ",
+                            prompt="which turtle will win the race?")
+y_pos = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+colour = ["red", "yellow", "green", "blue", "orange", "purple"]
+for _ in range(0, 6):
+    tuktuk = t.Turtle(shape="turtle")
+    tuktuk.color(colour[_])
+    tuktuk.penup()
+    tuktuk.goto(x=-230, y=y_pos[_])
+    all_turtles.append(tuktuk)
 
-screen.listen()
-screen.onkey(key="space",fun=move_forward)
+if user_bet:
+    is_race_on = True
+while is_race_on:
+    for tuktuk in all_turtles:
+        if tuktuk.xcor() > 220:
+            is_race_on = False
+            wining_color = tuktuk.pencolor()
+            if wining_color == user_bet:
+                print(f"you won, {wining_color} is the winner")
+            else:
+                print(f"you lost, {wining_color} is the winner")
+        rand_dist = random.randint(3, 10)
+        tuktuk.forward(rand_dist)
+
+
 screen.exitonclick()
