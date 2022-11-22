921520f069c9c6461542741b43f6df5be63a110f
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-22 23:26:34 +0530

    modified:   Day 20/main.py

--- Day 20/main.py
+++ Day 20/main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_position = [(0,0),(-20,0),(-40,0)]
 segments = []
