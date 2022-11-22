7d5fdddf32edf850b2bbbf3b5cade04966a317b9
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-22 23:46:16 +0530

    modified:   Day 20/main.py

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,27 +1,31 @@
-from turtle import Screen , Turtle
+import time
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_position = [(0,0),(-20,0),(-40,0)]
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
-for i in range(0,3):
+for i in range(0, 3):
     snake_body = Turtle("square")
     snake_body.color("white")
     snake_body.penup()
-    snake_body.goto(-20*i,0)
+    snake_body.goto(-20*i, 0)
     segments.append(snake_body)
-    
+
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
-
-
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x,new_y)
+    segments[0].forward(20)
 
 
 screen.exitonclick()
