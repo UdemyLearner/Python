90c09baf53f77b16a65935c1cefd65e42c10c6a6
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-25 08:56:25 +0530

    modified:   Print.py

--- Print.py
+++ Print.py
@@ -1,6 +1,14 @@
-print("hello")
-for i in range(0,10):
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#for loop in python 
+for i in range(0,3):
     print(i)
+#if statment in python
 if 5>3:
-    print(5>3)        
-    print("ok")
+    print(5>3)
